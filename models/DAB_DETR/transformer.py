f17cd34360b9cd1bc155bc53a19051f76afa262a
damien911224 <damien911224@gmail.com>
2023-02-21 22:54:57 +0900

    add demo

--- models/DAB_DETR/transformer.py
+++ models/DAB_DETR/transformer.py
@@ -123,8 +123,6 @@ class Transformer(nn.Module):
     def forward(self, src, mask, refpoint_embed, pos_embed):
         # flatten NxCxHxW to HWxNxC
         bs, c, h, w = src.shape
-        print(h, w)
-        exit()
         src = src.flatten(2).permute(2, 0, 1)
         pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
         refpoint_embed = refpoint_embed.unsqueeze(1).repeat(1, bs, 1)
