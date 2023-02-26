1c8c77cb8557b17efeaa175b1e6048e71e601c13
damien911224 <damien911224@gmail.com>
2023-02-26 23:32:08 +0900

    add demo

--- models/DAB_DETR/transformer.py
+++ models/DAB_DETR/transformer.py
@@ -494,11 +494,11 @@ class TransformerDecoderLayer(nn.Module):
             layer_count += 1
             Q_weights = Q_weights[0].detach().cpu()
 
-            map = Q_weights.view(30, 30, 30, 30).mean(dim=0).sum(dim=1)
+            # map = Q_weights.view(30, 30, 30, 30).mean(dim=0).sum(dim=1)
             # map = Q_weights.view(3, 10, 10, 3, 10, 10).mean(dim=(0, 1)).sum(dim=(1, 2))
-            # map = Q_weights[:40, :40]
+            map = Q_weights[40:40 + 40, 40:40 + 40]
             # # map = K_weights.view(25, 34, 25, 34).mean(dim=1).sum(dim=2)
-            map = map / torch.sum(map, dim=-1, keepdim=True)
+            # map = map / torch.sum(map, dim=-1, keepdim=True)
             # map = K_weights
 
             map = map.numpy()
